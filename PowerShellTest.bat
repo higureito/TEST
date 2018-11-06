37e30e93d78ee83f64d2b52b65ec3f0302da43c2
higureito <gsdma5ster544giju10@gmail.com>
2018-11-07 01:55:56 +0900

    add new file

--- PowerShellTest.bat
+++ PowerShellTest.bat
@@ -1,18 +1,28 @@
 @setlocal enabledelayedexpansion&set a=%*&(if defined a set a=!a:"=\"!&set a=!a:'=''!)&powershell/c $i=$input;iex ('$i^|^&{$PSCommandPath=\"%~f0\";$PSScriptRoot=\"%~dp0";#'+(${%~f0}^|Out-String)+'} '+('!a!'-replace'[$(),;@`{}]','`$0'))&exit/b
 #StartPowerShellScript
 
-#$GetOS = [environment]::osversion.versionstring
-#"$GetOS"
+$OS = Get-WmiObject -Class Win32_OperatingSystem
 
-Switch ( [environment]::osversion.versionstring ) {
-	{$_ -like "*10.0.*" } {$GetOS = "Win10"}
-	{$_ -like "*6.3.*" } {$GetOS = "Win8.1"}
-	{$_ -like "*6.2.*" } {$GetOS = "Win8"}
-	{$_ -like "*6.1.*" } {$GetOS = "Win7"}
-	{$_ -like "*6.0.*" } {$GetOS = "WinVista"}
+"$OS"
+
+Switch ( $OS.Version ) {
+	{$_ -match "10.0." } {$GetOS = "Win10"}
+	{$_ -match "6.3." } {$GetOS = "Win8.1"}
+	{$_ -match "6.2." } {$GetOS = "Win8"}
+	{$_ -match "6.1." } {$GetOS = "Win7"}
+	{$_ -match "6.0." } {$GetOS = "WinVista"}
 	default {$GetOS = "UnknownOS"}
 		}
+
 "$GetOS"
+
+Switch ( $OS.OSArchitecture ) {
+	{$_ -match "64" } {$GetBit = "64-Bit"}
+	{$_ -match "32" } {$GetBit = "32-Bit"}
+	default {$GetBit = "UnknownOS"}
+		}
+
+"$GetBit"
 
 Write-Host "Press any key to continue ..."
 $host.UI.RawUI.ReadKey()
